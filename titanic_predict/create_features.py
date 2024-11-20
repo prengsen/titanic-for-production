6e5d7af5e2fafe8871218ad7c52e624a0d10dfb6
Preng Biba <prengsen@galileo.edu>
2024-11-19 18:20:13 -0600

    agregams train y evaluate

--- titanic_predict/create_features.py
+++ titanic_predict/create_features.py
@@ -3,7 +3,7 @@
 """
 import pickle
 import pandas as pd
-
+import numpy as np
 
 def create_model_features():
     """
@@ -15,23 +15,36 @@ def create_model_features():
     # eliminamos cabin por cantidad de faltantes.
     dataset.drop(['Cabin'], axis=1, inplace=True)
 
+    # Tamaño del conjunto de prueba (por ejemplo, 20%)
+    test_size = 0.2
+
+    # Obtener el número de filas
+    num_rows = len(dataset)
+
+    # Crear índices aleatorios para el conjunto de prueba
+    test_indices = np.random.choice(num_rows, int(num_rows * test_size), replace=False)
+
+    # Crear un DataFrame booleano para indicar si una fila pertenece al conjunto de prueba
+    is_test = np.isin(np.arange(num_rows), test_indices)
+
+    # Dividir el DataFrame
+    train = dataset.loc[~is_test]
+    test = dataset.loc[is_test]
+
     # Imputamos la media en la columna Age
-    media_age = dataset['Age'].mean()
-    dataset['Age'] = dataset['Age'].fillna(media_age).astype(int)
+    media_age = train['Age'].mean()
+    train['Age'] = train['Age'].fillna(media_age).astype(int)
 
     # Imputamos Embarked con valor mas frecuente.
-    embarked_imputed_value = dataset['Embarked'].value_counts().index[0]
-    dataset['Embarked'] = dataset['Embarked'].fillna(embarked_imputed_value)
+    embarked_imputed_value = train['Embarked'].value_counts().index[0]
+    train['Embarked'] = train['Embarked'].fillna(embarked_imputed_value)
 
     # Codificación de variable Sex con One Hot Encoding
-    dataset['Sex'] = pd.get_dummies(dataset['Sex'], drop_first=True).astype(int)
+    train['Sex'] = pd.get_dummies(train['Sex'], drop_first=True).astype(int)
 
     # Codificación de variable Embarked con Frecuency Encoding
-    codificador_embarked = dataset['Embarked'].value_counts()
-    dataset['Embarked'] = dataset['Embarked'].map(codificador_embarked)
-
-    # Guardamos el Dataset Procesado.
-    dataset.to_csv('../data/processed/features_for_model.csv', index=False)
+    codificador_embarked = train['Embarked'].value_counts()
+    train['Embarked'] = train['Embarked'].map(codificador_embarked)
 
 # Gurdamos valores de configuración del train.
     feature_eng_configs = {
@@ -42,3 +55,9 @@ def create_model_features():
 
     with open('../artifacts/feature_eng_configs.pkl', 'wb') as f:
         pickle.dump(feature_eng_configs, f)
+
+    # Guardamos el Dataset Procesado.
+    train.to_csv('../data/processed/features_for_model.csv', index=False)
+    test.to_csv('../data/processed/test_dataset.csv', index=False)
+#llamada a función para ajecución.
+create_model_features()
---
+++ titanic_predict/train.py
@@ -0,0 +1,64 @@
+"""
+_summary_: este código genera los modelos de clasificación para el problema de titanic.
+"""
+
+import pickle
+import pandas as pd
+
+# sklearn tools
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import accuracy_score
+
+# modelos
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
+
+
+def train_model():
+    """
+        _summary_: esta función entrana los modelos de clasificación y almacena el mejor.
+    """
+    # Cargamos los datos
+    dataset = pd.read_csv('../data/processed/features_for_model.csv')
+
+    # Seleccionar el Target y Features
+    x = dataset.drop(['Survived'], axis=1)
+    y = dataset['Survived']
+
+    # Split en train y test.
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3,
+                                                        shuffle=True, random_state=2025)
+
+    # Configuramos scaler
+    std_scaler = StandardScaler()
+    std_scaler.fit(x_train)  # calculamos valores de entrenamiento.
+
+    with open('../artifacts/std_scaler.pkl', 'wb') as f:
+        pickle.dump(std_scaler, f)
+
+    # aplicamos scaler a datos de entrenamiento.
+    x_train_std = std_scaler.transform(x_train)
+
+    # entrenamos modelo de random forest
+    modelo_rf = RandomForestClassifier()
+    modelo_rf.fit(x_train_std, y_train)
+
+    x_test_std = std_scaler.transform(x_test)
+    y_preds_rf = modelo_rf.predict(x_test_std)
+    rf_acc = accuracy_score(y_test, y_preds_rf)
+
+    # Regresión Logística
+    modelo_rl = LogisticRegression()
+    modelo_rl.fit(x_train_std, y_train)
+
+    y_preds_rl = modelo_rl.predict(x_test_std)
+    rl_acc = accuracy_score(y_test, y_preds_rl)
+
+    if (rf_acc >= rl_acc):
+        winner_model = modelo_rf
+    else:
+        winner_model = modelo_rl
+
+    with open('../models/titanic_model_v1.pkl', 'wb') as f:
+        pickle.dump(winner_model, f)
